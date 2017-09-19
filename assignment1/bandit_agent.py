d673d989a88c17f2c04def7961af7efb124a766b
Carson McLean <camclean@ualberta.ca>
2017-09-19 15:36:24 -0600

    Creating assignment files by copying sample code

---
+++ assignment1/bandit_agent.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+"""
+  Author: Adam White, Matthew Schlegel, Mohammad M. Ajallooeian
+  Purpose: for use of Rienforcement learning course University of Alberta Fall 2017
+ 
+  agent does *no* learning, selects actions randomly from the set of legal actions
+ 
+"""
+
+from utils import rand_in_range
+import numpy as np
+
+last_action = None # last_action: NumPy array
+
+num_actions = 1
+
+def agent_init():
+    global last_action
+
+    last_action = np.zeros(1) # generates a NumPy array with size 1 equal to zero
+
+def agent_start(this_observation): # returns NumPy array, this_observation: NumPy array
+    global last_action
+
+    last_action[0] = rand_in_range(num_actions)
+
+    local_action = np.zeros(1)
+    local_action[0] = rand_in_range(num_actions)
+
+    return local_action[0]
+
+
+def agent_step(reward, this_observation): # returns NumPy array, reward: floating point, this_observation: NumPy array
+    global last_action
+
+    local_action = np.zeros(1)
+    local_action[0] = rand_in_range(num_actions)
+
+    # might do some learning here
+
+    last_action = local_action
+
+    return last_action
+
+def agent_end(reward): # reward: floating point
+    # final learning update at end of episode
+    return
+
+def agent_cleanup():
+    # clean up
+    return
+
+def agent_message(inMessage): # returns string, inMessage: string
+    # might be useful to get information from the agent
+
+    if inMessage == "what is your name?":
+        return "my name is skeleton_agent!"
+  
+    # else
+    return "I don't know how to respond to your message"
---
+++ assignment1/bandit_exp.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""
+  Author: Adam White, Matthew Schlegel, Mohammad M. Ajallooeian
+  Purpose: for use of Rienforcement learning course University of Alberta Fall 2017
+  Last Modified by: Andrew Jacobsen, Victor Silva, Mohammad M. Ajallooeian
+  Last Modified on: 16/9/2017
+
+  Experiment runs 2000 runs, each 1000 steps, of an n-armed bandit problem
+"""
+
+from rl_glue import *  # Required for RL-Glue
+RLGlue("simple_env", "simple_agent")
+
+import numpy as np
+import sys
+
+def save_results(data, data_size, filename): # data: floating point, data_size: integer, filename: string
+    with open(filename, "w") as data_file:
+        for i in range(data_size):
+            data_file.write("{0}\n".format(data[i]))
+
+if __name__ == "__main__":
+    num_runs = 2000
+    max_steps = 1000
+
+    # array to store the results of each step
+    optimal_action = np.zeros(max_steps)
+
+    print "\nPrinting one dot for every run: {0} total Runs to complete".format(num_runs)
+    for k in range(num_runs):
+        RL_init()
+
+        RL_start()
+        for i in range(max_steps):
+            # RL_step returns (reward, state, action, is_terminal); we need only the
+            # action in this problem
+            action = RL_step()[2]
+
+            '''
+            check if action taken was optimal
+
+            you need to get the optimal action; see the news/notices
+            announcement on eClass for how to implement this
+            '''
+            # update your optimal action statistic here
+
+        RL_cleanup()
+        print ".",
+        sys.stdout.flush()
+
+    save_results(optimal_action / num_runs, max_steps, "RL_EXP_OUT.dat")
+    print "\nDone"
