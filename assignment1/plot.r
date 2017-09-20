b62b5c92d59a3192b295ad893ca5bc1d1daa4510
Carson McLean <camclean@ualberta.ca>
2017-09-19 22:29:40 -0600

    Only make one line on graph at time. Easier for submission.

--- assignment1/plot.r
+++ assignment1/plot.r
@@ -3,10 +3,12 @@
 # Author: Matthew Schlegel
 # Purpose: Just a simple plotting util to read in data from RL_EXP_OUT.dat
 
-dat = read.table("RL_EXP_OUT01.dat")
+dat = read.table("RL_EXP_OUT.dat")
 
 plot(x = 1 : length(dat[, 1]), y = dat[,1], type = "l", ylab = "% Optimal Action", xlab = "Steps", col = "purple", ylim=c(0,1))
 
-dat = read.table("RL_EXP_OUT0.dat")
+# Uncomment below to plot two lines on the same graph. 
+# Requires you to change the data file out name in w1_exp.py near the end
+# dat = read.table("RL_EXP_OUT1.dat")
 
-lines(x = 1 : length(dat[, 1]), y = dat[,1], type = "l", col = "red")
+# lines(x = 1 : length(dat[, 1]), y = dat[,1], type = "l", col = "red")
