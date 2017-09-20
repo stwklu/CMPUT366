8411aeaafc2bc0e6ef02c7ceac7930365466b3d6
Carson McLean <camclean@ualberta.ca>
2017-09-19 18:41:04 -0600

    Two line graphs

--- assignment1/plot.r
+++ assignment1/plot.r
@@ -3,6 +3,10 @@
 # Author: Matthew Schlegel
 # Purpose: Just a simple plotting util to read in data from RL_EXP_OUT.dat
 
-dat = read.table("RL_EXP_OUT.dat")
+dat = read.table("RL_EXP_OUT01.dat")
 
-plot(x = 1 : length(dat[, 1]), y = dat[,1], type = "l", ylab = "% Optimal Action", xlab = "Steps", col = "purple")
+plot(x = 1 : length(dat[, 1]), y = dat[,1], type = "l", ylab = "% Optimal Action", xlab = "Steps", col = "purple", ylim=c(0,1))
+
+dat = read.table("RL_EXP_OUT0.dat")
+
+lines(x = 1 : length(dat[, 1]), y = dat[,1], type = "l", col = "red")
