5bba36b2e82aaa491ec4ff42ac31fa3d5b54f749
Carson McLean <camclean@ualberta.ca>
2017-10-07 23:41:48 -0600

    Progress on value iteration function

--- assignment3/gambler.py
+++ assignment3/gambler.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 
+import numpy as np
 
-def value_iteration():
+def value_iteration(values):
+  delta = 0.0
+  cutoff = 1**-6
+
+  while delta > cutoff :
+    for i in range (len(values)):
+      v = values[i]
+      values[i] = max_action()
+      delta = max(delta, v - values[i])
+
+
+def max_action():
   pass
 
 def main():
-  value_iteration()
+  values = np.zeros(101)
+  values[100] = 1.0
+
+  value_iteration(values)
 
 if __name__ == '__main__':
   main()
