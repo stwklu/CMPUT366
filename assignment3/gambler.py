c2ef134e67fc8c3e39c5a99c172318eb84aeff6d
Carson McLean <camclean@ualberta.ca>
2017-10-08 01:44:10 -0600

    Values starting to be returned

--- assignment3/gambler.py
+++ assignment3/gambler.py
@@ -2,25 +2,55 @@
 
 import numpy as np
 
-def value_iteration(values):
-  delta = 0.0
-  cutoff = 1**-6
+### PARAMETERS ###
+# Probability of heads 
+ph = 0.4
+### PARAMETERS ###
 
-  while delta > cutoff :
-    for i in range (len(values)):
-      v = values[i]
-      values[i] = max_action()
-      delta = max(delta, v - values[i])
+def value_iteration(values, policy):
+  delta = 100.0
+  cutoff = 0.1
+
+  # while delta > cutoff :
+  for i in range (1,100):
+    v = values[i]
+    values[i] = max_action(values, policy, i)
+    # print(v)
+    # print(values)
+    delta = max(delta, v - values[i])
+    # print(delta)
 
 
-def max_action():
-  pass
+def max_action(values, policy, i):
+  v_kplus1 = 0.0
+  max_value = 0.0
+
+  # Try every action possible from current state i
+  for action in range(0, i):
+    # Does the action achieve our goal from our current state?
+    if (i>98):
+      print("act+i")
+      print(action+i)
+    if (action + i) >= 100:
+      print("heeeeerrr")
+      # Reward of 1.0 for reaching goal off of a heads
+      v_kplus1 = (ph * (1.0 + values[i])) + ((1.0 - ph) * (0.0 + values[i])) 
+    else:
+      v_kplus1 = (ph * (0.0 + values[i])) + ((1.0 - ph) * (0.0 + values[i]))
+
+    if v_kplus1 > max_value:
+      max_value = v_kplus1
+  
+  return max_value
 
 def main():
   values = np.zeros(101)
   values[100] = 1.0
+  policy = np.zeros(101)
 
-  value_iteration(values)
+  value_iteration(values, policy)
+
+  print(values)
 
 if __name__ == '__main__':
   main()
