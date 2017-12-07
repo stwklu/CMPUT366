e8015a0e79411a2c86bbc66109e785b62b09debc
Carson McLean <camclean@ualberta.ca>
2017-12-06 18:50:45 -0700

    Starting part3

---
+++ assignment7/part3/mountaincar_exp.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+  Author: Adam White, Matthew Schlegel, Mohammad M. Ajallooeian, Andrew
+  Jacobsen, Victor Silva, Sina Ghiassian
+  Purpose: for use of Rienforcement learning course University of Alberta Fall 2017
+  Last Modified by: Mohammad M. Ajallooeian, Sina Ghiassian
+  Last Modified on: 21/11/2017
+
+"""
+
+from rl_glue import *  # Required for RL-Glue
+RLGlue("mountaincar", "sarsa_lambda_agent")
+
+import numpy as np
+
+if __name__ == "__main__":
+    num_episodes = 1000
+    num_runs = 1
+
+    steps = np.zeros([num_runs,num_episodes])
+
+    for r in range(num_runs):
+        print "run number : ", r
+        RL_init()
+        for e in range(num_episodes):
+            # print '\tepisode {}'.format(e+1)
+            RL_episode(0)
+            steps[r,e] = RL_num_steps()
+    weights = RL_agent_message("3d")
+    np.save('steps',steps)
